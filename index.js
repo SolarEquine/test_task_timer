d222841321b8772e8c7fd5b89e414cd6b844ba13
Tia <nik12346@yandex.ru>
2022-11-18 17:27:16 +0300

    Timer countdown done

--- index.js
+++ index.js
@@ -2,11 +2,31 @@ const inputEl = document.querySelector('input');
 const buttonEl = document.querySelector('button');
 const timerEl = document.querySelector('span');
 
+let timer = 0;
 // Напишите реализацию createTimerAnimator
 // который будет анимировать timerEl
 const createTimerAnimator = () => {
-  return (seconds) => {};
+  return (seconds) => {
+    clearInterval(timer);
+    timerEl.innerHTML = parseTime(seconds);
+    timer = setInterval(()=>{
+      seconds-=1;
+      timerEl.innerHTML = parseTime(seconds);
+      if (seconds<=0){
+        clearInterval(timer);
+      }
+    }, 1000);
+  };
 };
+
+function parseTime(seconds){
+  let parsedHours = Math.floor(seconds/60/60);
+  let parsedMinutes = Math.floor(seconds/60)%60;
+  let parsedSeconds = seconds%60;
+  let parsedTime = "";
+  parsedTime = parsedHours +":"+parsedMinutes+":"+parsedSeconds;
+  return parsedTime
+}
 
 const animateTimer = createTimerAnimator();
 
